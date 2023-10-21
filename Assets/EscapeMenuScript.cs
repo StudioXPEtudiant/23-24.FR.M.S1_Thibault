c56afd5215946329668243a945d32569964e0172
Artenis67 <artenis67eininger@gmail.com>
2023-10-21 15:18:26 +0200

    Ajustement du PlayerUI et mise en place de particules
    Ajout {
    - Les boutons du Player UI sont fonctionnels
    - Particules de poison
    - Particules de Fumée
    - Particules de feuille qui tombe
    }
    
    Fix {
    - Fix du Player UI qui se met maintenant sur tout l'écran
    }

--- Assets/EscapeMenuScript.cs
+++ Assets/EscapeMenuScript.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class EscapeMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GameQuit()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
