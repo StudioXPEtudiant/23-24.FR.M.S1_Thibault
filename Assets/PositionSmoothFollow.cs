b5f71b367d861cadfa528a103efd88a0c8569d04
Artenis67 <artenis67eininger@gmail.com>
2023-10-23 15:14:22 +0200

    Ajout d'un Système de suivis de Camera
    Ajouts {
    - Script de suivi agréable du joueur par la caméra
    - Meilleure intégration du fond avec un suivi
    }
    
    Autres {
    - Implémentation de plusieurs fonctionnalités dans la scène principale
    }

---
+++ Assets/PositionSmoothFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSmoothFollow : MonoBehaviour
+{
+    [Header("Camera Follow System")]
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -4f);
+    [SerializeField] private float smoothTime = 0.15f;
+    [SerializeField] private Vector3 velocity = Vector3.zero;
+
+    [Header("GameObject")] 
+    [SerializeField] private Transform target;
+    
+    void Update()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
