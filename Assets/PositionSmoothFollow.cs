9bbce9c75e4f89d07c9f4fc85d0008b3f4e9c27c
Artenis67 <artenis67eininger@gmail.com>
2023-10-23 15:16:51 +0200

    Fix du tremblement du player
    Fix {
    - Patch du bug de tremblement du joueur
    }

--- Assets/PositionSmoothFollow.cs
+++ Assets/PositionSmoothFollow.cs
@@ -12,7 +12,7 @@ public class PositionSmoothFollow : MonoBehaviour
     [Header("GameObject")] 
     [SerializeField] private Transform target;
     
-    void Update()
+    void FixedUpdate()
     {
         Vector3 targetPosition = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
